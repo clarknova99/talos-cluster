9301922436d655005ea58be5712c75606f40e15e
michael <michael@feldblyum.com>
2025-01-10 01:09:20 +0000

    bazaar

--- kubernetes/apps/media/bazarr/app/resources/subcleaner.sh
+++ kubernetes/apps/media/bazarr/app/resources/subcleaner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 printf "Cleaning subtitles for '%s' ...\n" "$1"
-python3 /add-ons/subcleaner/subcleaner.py "$1" -s
+python3 /subcleaner/subcleaner/subcleaner.py "$1" -s
 
 case $1 in
     *movies*) section="1";;
@@ -12,7 +12,7 @@ if [[ -n "$section" ]]; then
     printf "Refreshing Plex section '%s' for '%s' ...\n" "$section" "$(dirname "$1")"
     /usr/bin/curl -I -X GET -G \
         --data-urlencode "path=$(dirname "$1")" \
-        --data-urlencode "X-Plex-Token=${SECRET_PLEX_TOKEN}" \
+        --data-urlencode "X-Plex-Token=${PLEX_TOKEN}" \
         --no-progress-meter \
-            "http://plex.media.svc.cluster.local:32400/library/sections/${section}/refresh"
+            "http://plex.default.svc.cluster.local:32400/library/sections/${section}/refresh"
 fi
