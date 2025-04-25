3ce8f6963d0c6cfea00b60506fd33abe27b61cda
michael <michael@feldblyum.com>
2025-04-25 17:15:56 +0000

    kromgo

---
+++ .github/scripts/kromgo-badges.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import requests
+
+secret_domain = os.environ.get("SECRET_DOMAIN")
+
+def build_kromgo_url(tag: str, base_url: str = secret_domain):
+    print(f"Building kromgo {tag} url", file=sys.stdout)
+    return f"https://kromgo.{secret_domain}/{tag}?format=badge&style=flat-square"
+
+
+def download_svg(tag: str):
+    response = requests.get(build_kromgo_url(tag))
+    print(f"Downloaded badge {tag} with status: {response.status_code}", file=sys.stdout)
+
+    response.raise_for_status()
+
+    with open(f"./kromgo/{tag}.svg", "wb") as file_descriptor:
+        print(f"Saving badge {tag}", file=sys.stdout)
+
+        for chunk in response:
+            file_descriptor.write(chunk)
+
+
+if __name__ == "__main__":
+
+    for tag in [
+        "talos_version",
+        "kubernetes_version",
+        "cluster_age_days",
+        "cluster_node_count",
+        "cluster_cpu_core_total",        
+        "cluster_memory_total",
+        "cluster_cpu_usage",
+        "cluster_memory_usage"
+    ]:
+        try:
+            download_svg(tag)
+        except:
+            print(f"Downloading badge {tag} failed.", file=sys.stderr)
